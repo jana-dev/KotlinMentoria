1379dfd4078b2c1ff9eaecf3f1e10358efbc15bc
Janaina Tavares <52297249+jana-dev@users.noreply.github.com>
2024-09-10 15:40:41 -0300

    Mais atividades adicionadas de Orientação a Objetos com Kotlin

--- app/src/main/java/com/example/kotlinmentoria/Sobrecarga.kt
+++ app/src/main/java/com/example/kotlinmentoria/Sobrecarga.kt
@@ -1,22 +1,22 @@
 package com.example.kotlinmentoria
 
-class Usuario {
-
-    //Propriedades
-
-    //Métodos
-    fun logar(email: String, senha: String){
-        println("Logando com email: $email e senha: $senha")
-    }
-
-    //Sobrecarga de métodos: assinatura nome + parametros e seus tipos
-    fun logar(numeroTelefone: String){
-        println("Logando com o número de telefone: $numeroTelefone")
-    }
-}
+//class Usuario {
+//
+//    //Propriedades
+//
+//    //Métodos
+//    fun logar(email: String, senha: String){
+//        println("Logando com email: $email e senha: $senha")
+//    }
+//
+//    //Sobrecarga de métodos: assinatura nome + parametros e seus tipos
+//    fun logar(numeroTelefone: String){
+//        println("Logando com o número de telefone: $numeroTelefone")
+//    }
+//}
 
 fun main(){
-    val usuario = Usuario()
-    //usuario.logar("teste@email.com", "1234")
-    usuario.logar("465684654") //ele executa o método sobrecarregador, de acordo com o parametro passado
+//    val usuario = Usuario()
+//    //usuario.logar("teste@email.com", "1234")
+//    usuario.logar("465684654") //ele executa o método sobrecarregador, de acordo com o parametro passado
 }
